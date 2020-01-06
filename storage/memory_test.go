c425962be498630ae7623c879b41ea3fc50ed25b
Carl Hester <crucialcarl@gmail.com>
2020-01-06 22:54:59 +0000

    formatting

--- storage/memory_test.go
+++ storage/memory_test.go
@@ -6,22 +6,22 @@ import "testing"
 import "fmt"
 
 func TestGetAllStations(t *testing.T) {
-	var stationRepository stations.Repository
-	stationRepository = NewMemoryStationStorage()
+	stationRepository := NewMemoryStationStorage()
+
 	have, _ := stationRepository.GetAllStations()
 	want := []stations.Station{{Abbr: "MONT", Name: "Montgomery"}}
+
 	if (have[0].Name != want[0].Name) || (have[0].Abbr != want[0].Abbr) {
 		t.Error(have[0], want[0])
 	}
 }
 
 func TestGetStationByAbbr(t *testing.T) {
-	var stationRepository stations.Repository
-	stationRepository = NewMemoryStationStorage()
+	stationRepository := NewMemoryStationStorage()
+
 	have, _ := stationRepository.GetStationByAbbr("MONT")
 	want := stations.Station{Abbr: "MONT", Name: "Montgomery"}
 
-	fmt.Println(have)
 	if have != want {
 		t.Error(have, want)
 	}
